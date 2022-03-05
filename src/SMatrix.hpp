5be19951b48437c2cf3d7de5135cb7ceb586bc1f
nR3D <nR3D@pm.me>
2022-03-05 12:35:34 +0100

    Refactored SMatrix as a std::array subclass

--- src/SMatrix.hpp
+++ src/SMatrix.hpp
@@ -5,10 +5,9 @@
 #include <array>
 
 template<typename T, std::size_t N, std::size_t ...O>
-class SMatrix {
+class SMatrix : public std::array<SMatrix<T, O...>, N> {
 private:
     static constexpr std::size_t D = sizeof...(O) + 1;
-    std::array<SMatrix<T, O...>,N> _container;
 
     template<std::size_t ...Idx>
     constexpr static std::size_t get_idx(std::size_t num, std::size_t max) {
@@ -20,6 +19,7 @@ private:
     struct SMatrix_reducer {};
 
     template<std::size_t ...Seq, std::size_t ...Idx>
+    requires (sizeof...(Seq) > 0)
     struct SMatrix_reducer<std::index_sequence<Seq...>, Idx...> {
         using type = SMatrix<T, get_idx<Idx...>(Seq, sizeof...(Seq)-1)...>;
     };
@@ -35,45 +35,45 @@ public:
     template<typename U, std::size_t M1, std::size_t M2>
     friend std::ostream &operator<<(std::ostream &, const SMatrix<U, M1, M2> &);
 
+    SMatrix() = default;
+
+    template<typename ...U>
+    requires (std::is_convertible_v<U,T> && ...)
+    SMatrix(const U& ...values) : std::array<SMatrix<T, O...>, N>({values...}) {}
+    
     template<std::integral Idx, std::integral... Indices>
     requires (sizeof...(Indices) == D-1)
     T& operator()(Idx index, Indices... indices) {
-        return _container[index](indices...);
+        return this->at(index)(indices...);
     }
 
     template<std::integral Idx, std::integral... Indices>
     requires (sizeof...(Indices) == D-1)
     const T& operator()(Idx index, Indices... indices) const {
-        return _container[index](indices...);
+        return this->at(index)(indices...);
     }
 
     template<std::integral Idx, std::integral... Indices>
     requires (sizeof...(Indices) < D-1) && (sizeof...(Indices) > 0)
     SMatrix_reduced_t<D - sizeof...(Indices) - 1, N, O...>& operator()(Idx index, Indices... indices) {
-        return _container[index](indices...);
+        return this->at(index)(indices...);
     }
 
     template<std::integral Idx, std::integral... Indices>
     requires (sizeof...(Indices) < D-1) && (sizeof...(Indices) > 0)
     const SMatrix_reduced_t<D - sizeof...(Indices) - 1, N, O...>& operator()(Idx index, Indices... indices) const {
-        return _container[index](indices...);
+        return this->at(index)(indices...);
     }
 
     SMatrix_reduced_t<D - 1, N, O...>& operator()(std::size_t index) {
-        return _container[index];
+        return this->at(index);
     }
 
     const SMatrix_reduced_t<D - 1, N, O...>& operator()(std::size_t index) const {
-        return _container[index];
+        return this->at(index);
     }
 
-    void set(std::array<std::size_t, D> indices, const T& value) {
-        std::array<std::size_t, D-1> next_indices;
-        std::copy(indices.begin() + 1, indices.end(), next_indices.begin());
-        _container[indices[0]].set(next_indices, value);
-    }
-
-    constexpr std::size_t size() const {
+    constexpr std::size_t total() const {
         return N * (O * ...);
     }
 
@@ -85,17 +85,17 @@ public:
 template<typename U, std::size_t M, size_t... P>
 requires (sizeof...(P) > 0)
 std::ostream &operator<<(std::ostream &os, const SMatrix<U, M, P...> &matrix) {
-    auto size = matrix._container.size();
+    auto size = matrix.size();
     os << "SMatrix[" << matrix.D << "]<";
     auto shape = matrix.shape();
     for(std::size_t i = 0; i < matrix.D; ++i) {
         os << shape.at(i);
         if(i < matrix.D-1)
-            os << ",";
+            os << ',';
     }
     os << ">{\n";
     for(std::size_t i = 0; i < size; ++i) {
-        os << matrix._container[i];
+        os << matrix.at(i);
         if(i < size-1)
             os << ",\n\n";
     }
@@ -105,34 +105,36 @@ std::ostream &operator<<(std::ostream &os, const SMatrix<U, M, P...> &matrix) {
 template<typename U, std::size_t M1, std::size_t M2>
 std::ostream &operator<<(std::ostream &os, const SMatrix<U, M1, M2> &matrix) {
     for(std::size_t i = 0; i < M1; ++i) {
-        os << matrix._container[i];
+        os << matrix.at(i);
         if(i < M1-1)
-            os << "\n";
+            os << '\n';
     }
     return os;
 }
 
 template<typename T, std::size_t N>
-class SMatrix<T,N> {
-private:
-    std::array<T,N> _container;
+class SMatrix<T,N> : public std::array<T,N> {
 public:
+    using std::array<T,N>::array;
+
     template<typename U, std::size_t M>
     friend std::ostream &operator<<(std::ostream &, const SMatrix<U, M> &);
 
+    SMatrix() = default;
+
+    template<typename ...U>
+    requires (std::is_convertible_v<U,T> && ...)
+    SMatrix(const U& ...values) : std::array<T, N>({values...}) {}
+
     T& operator()(std::size_t index) {
-        return _container[index];
+        return this->at(index);
     }
 
     T operator()(std::size_t index) const {
-        return _container[index];
+        return this->at(index);
     }
 
-    void set(std::array<std::size_t,1> index, const T& value) {
-        _container[index[0]] = value;
-    }
-
-    constexpr std::size_t size() const {
+    constexpr std::size_t total() const {
         return N;
     }
 
@@ -143,13 +145,13 @@ public:
 
 template<typename U, std::size_t M>
 std::ostream &operator<<(std::ostream & os, const SMatrix<U, M> &matrix) {
-    os << "|";
+    os << '|';
     for(std::size_t i = 0; i < M; ++i) {
-        os << matrix._container[i];
+        os << matrix.at(i);
         if(i < M-1)
             os << ", ";
     }
-    return os << "|";
+    return os << '|';
 }
 
 
